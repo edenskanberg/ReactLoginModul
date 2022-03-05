e056fd6f34ca1634f622387f88f1df3d6102dd2f
Eden Skånberg <eden.89@live.com>
2022-03-05 12:10:29 +0100

    ..

--- minapp/src/App.js
+++ minapp/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import FormSignup from './FormSignup';
 
-
-
 function App() {
   return (
     <div className="App">
       <FormSignup />
     </div>
   );
-}
+};
 
 export default App;
